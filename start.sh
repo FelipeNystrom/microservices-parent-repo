a2b79fb5027543a7dfe58cc969373bebbda763c1
Felipe Nystrom <andre.felipe.nystrom@gmail.com>
2019-06-25 22:26:42 +0200

    add start script

---
+++ start.sh
@@ -0,0 +1,21 @@
+kubectl apply -f resources
+
+kubectl apply -f resources/secrets
+
+kubectl create configmap db-config --from-file=./resources/user_service.sql --from-file=./resources/post_service.sql
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+
+minikube addons enable ingress 
+
+kubectl apply -f https://raw.githubusercontent.com/rook/rook/master/cluster/examples/kubernetes/ceph/common.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/rook/rook/master/cluster/examples/kubernetes/ceph/operator.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/rook/rook/master/cluster/examples/kubernetes/ceph/cluster-minimal.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/rook/rook/master/cluster/examples/kubernetes/ceph/storageclass-test.yaml
+
+kubectl apply -f statefulSets
+
+kubectl apply -f services
